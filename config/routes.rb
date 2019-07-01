0427cec2fe5b56ccdd667c1d00ef2b62b62b23f1
pablo <pabloperezpastene@yahoo.es>
2019-07-01 02:51:39 -0400

    Primer commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about'
+  resources :parents
+  resources :teachers
+  resources :students
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+end
